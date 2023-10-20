4b81b3a68b995fb7a0b7130fdd7525a43cdc195b
Gaetan Moreau <gmoreau.contact@gmail.com>
2023-10-20 16:28:34 +0200

    Add pokemon page and refacto code

--- app/utils/getRandomPokemon.tsx
+++
@@ -1,6 +0,0 @@
-import pokemons from "../assets/data/pokemons.json";
-
-export const getRandomPokemon = () => {
-  const randomPokemon = pokemons[Math.floor(Math.random() * pokemons.length)];
-  return randomPokemon;
-}
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  v2_routeConvention: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
